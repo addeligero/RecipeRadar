453d7ce74f16a850bc0615146f0ad9d17e1a7abb
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-26 11:40:53 +0800

    styles: added in dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -22,14 +22,23 @@ const image = '/images/1.png';
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+            <div
+                class="relative flex min-h-[100vh] flex-1 items-center justify-center rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min"
+            >
                 <img :src="image" alt="My Image" />
             </div>
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <CardContent>
                         <Card>
-                            <CardHeader><h1 class="text-center">hi</h1></CardHeader>
+                            <CardHeader
+                                ><button
+                                    type="button"
+                                    class="mb-2 me-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                                >
+                                    Search now
+                                </button></CardHeader
+                            >
                             <CardDescription>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eaque beatae fugit minima tempore asperiores est deserunt,
                                 soluta, recusandae nobis veritatis eveniet? In totam delectus excepturi amet repellat quasi exercitationem.
