734affd74bd83c5e07e20180d048a019dc80610a
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-27 10:45:06 +0800

    fixed: image and button  positioning in dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -21,12 +21,12 @@ const image = '/images/1.png';
             <div
                 class="relative flex min-h-[100vh] flex-1 items-center justify-center rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min"
             >
-                <img :src="image" alt="My Image" />
+                <img class="opacity-80" :src="image" alt="My Image" />
             </div>
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <div class="relative flex h-full flex-col items-center justify-center p-5">
-                        <img class="absolute inset-0 h-full w-full object-cover opacity-50" src="/images/pan.png" alt="" />
+                        <img class="absolute h-full w-full opacity-75" src="/images/foods.png" alt="" />
                         <button
                             type="button"
                             class="relative z-10 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
