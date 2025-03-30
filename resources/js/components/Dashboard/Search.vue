9285e323e5d3853e4152cae51608280fb520b2d9
ADRIAN <adriandeligero.1030@gmail.com>
2025-03-30 20:12:22 +0800

    fixed: remove and replacing the search to new component

--- resources/js/components/Dashboard/Search.vue
+++ resources/js/components/Dashboard/Search.vue
@@ -8,7 +8,7 @@ import { Link } from '@inertiajs/vue3';
             <img class="absolute h-full w-full opacity-75" src="/images/foods.png" alt="" />
 
             <Link
-                href="/search"
+                href="/searched"
                 class="relative z-10 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
             >
                 Search now
